e2d5244d33698037abd43807cbe6a2bd1cc6deb4
Thinus <thinusvdv2001@gmail.com>
2022-12-01 12:23:13 +0200

    Add script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const HamLine1 = document.querySelector('.HamLine1');
+const HamLine2 = document.querySelector('.HamLine2');
+const HamLine3 = document.querySelector('.HamLine3');
+const Change1 = document.querySelector('.Change1');
+const Change2 = document.querySelector('.Change2');
+const Change3 = document.querySelector('.Change3');
 const mobileMenu = document.querySelector('.MobileMenu');
 const hamburgerButton = document.querySelector('.hamburger');
 const hidden = document.querySelector('.hidden');
@@ -69,5 +75,11 @@ judgements.appendChild(section);
 `;
 
 hamburgerButton.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden')
+  mobileMenu.classList.toggle('hidden');
+  HamLine1.classList.toggle('Change1');
+  HamLine2.classList.toggle('Change2');
+  HamLine3.classList.toggle('Change3');
 });
+
+
+
