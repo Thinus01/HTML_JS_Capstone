eb669b872c67fcb25f1f1270894c700a562840d5
Thinus <thinusvdv2001@gmail.com>
2022-12-02 22:46:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,68 +7,86 @@ const judgements = document.querySelector('#Judgements');
 const section = document.createElement('section');
 judgements.appendChild(section);
 
-section.innerHTML = `
-  <section id="FeaturedSpeakers">
-  <h2 class="TopSix col-lg-3 offset-lg-4 col-8 col-sm-8 offset-sm-3 offset-3">Top six used warframes</h2>
-  <div class="col-lg-2 offset-lg-5 col-2 col-sm-2 offset-sm-5 offset-5"><hr class="Line col-lg-2 offset-lg-5 col-2 col-sm-2 offset-sm-5 offset-5"></div>
-  <ul class="nav_top row col-lg-12 col-12 col-sm-12">
-      <li class="Image col-lg-5 col-12 col-sm-12 offset-lg-1 row"> <img class="col-lg-5 col-5 col-sm-5" src="images/wukong.jpg" alt="wukong">
-          <ul class="nav_top col-lg-6 col-6 col-sm-7">
-          <li><h2 class="col-lg-12 col-12 col-sm-12">Wukong</h2></li>
-          <li><h3 class="col-lg-12 col-12 col-sm-12">Comming in at number one</h3></li>
-          <li><p class="Desc col-lg-12 col-12 col-sm-12">With wukongs celestial twin, he is able to cover both melee and ranged attacks and his cloud walk makes him unstopable when it comes to spy missions.</p></li>
-      </ul></li>
-      <li class="Image col-lg-5 col-12 col-sm-12 row"> <img class="col-lg-5 col-5 col-sm-5" src="images/volt.jpg" alt="volt">
-          <ul class="nav_top col-lg-7 col-7 col-sm-7">
-          <li><h2 class="col-lg-12 col-12 col-sm-12">Volt</h2></li>
-          <li><h3 class="col-lg-12 col-12 col-sm-12">At number two</h3></li>
-          <li><p class="Desc col-lg-12 col-12 col-sm-12">Speed, damage, nuking and tanking...what more do I need to say.</p></li>
-      </ul></li>
-      <li class="Image col-lg-5 col-12 col-sm-12 offset-lg-1 row"> <img class="col-lg-5 col-5 col-sm-5" src="images/wisp.jpg" alt="wisp">
-          <ul class="nav_top col-lg-7 col-7 col-sm-7">
-          <li><h2 class="col-lg-12 col-12 col-sm-12">wisp</h2></li>
-          <li><h3 class="col-lg-12 col-12 col-sm-12">At number three</h3></li>
-          <li><p class="Desc col-lg-12 col-12 col-sm-12">Need to be healed, Wisp has your back. Need speed, Wisp has your back. Need electric damage, Wisp has your back. Need to be teleported, Wisp does not have your back. So selfish.</p></li>
-      </ul></li>
-      <li class="Image col-lg-5 col-12 col-sm-12 row"> <img class="col-lg-5 col-5 col-sm-5" src="images/Baruuk.jpg" alt="Baruuk">
-          <ul class="nav_top col-lg-7 col-7 col-sm-7">
-          <li><h2 class="col-lg-12 col-12 col-sm-12">Baruuk</h2></li>
-          <li><h3 class="col-lg-12 col-12 col-sm-12">At number four</h3></li>
-          <li><p class="Desc col-lg-12 col-12 col-sm-12">Corpus, Grineer, Infested, Corrupted, you name it, Brauuk has you covered, with his adaptive elemental damage he is ready for everything and everyone.Such a gental soul.</p></li>
-      </ul></li>
-      <li class="Image col-lg-5 col-12 col-sm-12 offset-lg-1 row"> <img class="col-lg-5 col-5 col-sm-5" src="images/mirage.jpg" alt="mirage">
-          <ul class="nav_top col-lg-7 col-7 col-sm-7">
-          <li><h2 class="col-lg-12 col-12 col-sm-12">mirage</h2></li>
-          <li><h3 class="col-lg-12 col-12 col-sm-12">At number five</h3></li>
-          <li><p class="Desc col-lg-12 col-12 col-sm-12">If damage negation and increased damage dealt is not enough...just summon a few clones and your good to go.She has a dico ball thats fun too, maybe not for the enemy but meh...</p></li>
-      </ul></li>
-      <li class="Image col-lg-5 col-12 col-sm-12 row"> <img class="col-lg-5 col-5 col-sm-5" src="images/saryn.jpg" alt="saryn">
-          <ul class="nav_top col-lg-7 col-7 col-sm-7">
-          <li><h2 class="col-lg-12 col-12 col-sm-12">saryn</h2></li>
-          <li><h3 class="col-lg-12 col-12 col-sm-12">At number six</h3></li>
-          <li><p class="Desc col-lg-12 col-12 col-sm-12">Saryn is not someone to be reckoned with. With her spores seeking enemies, making then take extra damage and miasma reducing their armor. Not a gril whos heart you want to break.</p></li>
-      </ul></li>
-  </ul>
-</section>
-<section id="MobileFeaturedSpeakers">
-<h2 class="TopSix col-lg-3 offset-lg-4 col-8 col-sm-8 offset-sm-3 offset-3">Top six used warframes</h2>
-<div class="col-lg-2 offset-lg-5 col-2 col-sm-2 offset-sm-5 offset-5"><hr class="Line col-lg-2 offset-lg-5 col-2 col-sm-2 offset-sm-5 offset-5"></div>
-<ul class="nav_top row col-lg-12 col-12 col-sm-12">
-    <li class="Image col-lg-5 col-12 col-sm-12 offset-lg-1 row"> <img class="col-lg-5 col-5 col-sm-5" src="images/wukong.jpg" alt="wukong">
-        <ul class="nav_top col-lg-6 col-6 col-sm-7">
-        <li><h2 class="col-lg-12 col-12 col-sm-12">Wukong</h2></li>
-        <li><h3 class="col-lg-12 col-12 col-sm-12">Comming in at number one</h3></li>
-        <li><p class="Desc col-lg-12 col-12 col-sm-12">With wukongs celestial twin, he is able to cover both melee and ranged attacks and his cloud walk makes him unstopable when it comes to spy missions.</p></li>
-    </ul></li>
-    <li class="Image col-lg-5 col-12 col-sm-12 row"> <img class="col-lg-5 col-5 col-sm-5" src="images/volt.jpg" alt="volt">
-        <ul class="nav_top col-lg-7 col-7 col-sm-7">
-        <li><h2 class="col-lg-12 col-12 col-sm-12">Volt</h2></li>
-        <li><h3 class="col-lg-12 col-12 col-sm-12">At number two</h3></li>
-        <li><p class="Desc col-lg-12 col-12 col-sm-12">Speed, damage, nuking and tanking...what more do I need to say.</p></li>
-    </ul></li>
-</ul>
-</section>
-`;
+const projectData = [
+    {
+      Warframe: 'Wukong',
+      Rank: '1',
+      Position: 'Comming in number one',
+      Images: 'images/wukong.jpg',
+      description: "With wukongs celestial twin, he is able to cover both melee and ranged attacks.",
+    },
+    {
+      Warframe: 'Volt',
+      Rank: '2',
+      Position: 'At number two',
+      Images: 'images/volt.jpg',
+      description: "Speed, damage, nuking and tanking...what more do I need to say.",
+    },
+    {
+      Warframe: 'Wisp',
+      Rank: '3',
+      Position: 'At number three',
+      Images: 'images/wisp.jpg',
+      description: "Need to be healed, Wisp has your back. Need speed, Wisp has your back. Need electric damage, Wisp has your back.",
+    },
+    {
+      Warframe: 'Baruuk',
+      Rank: '4',
+      Position: 'At number four',
+      Images: 'images/Baruuk.jpg',
+      description: "With his adaptive elemental damage he is ready for everything and everyone.",
+    },
+    {
+      Warframe: 'Mirage',
+      Rank: '5',
+      Position: 'At number five',
+      Images: 'images/mirage.jpg',
+      description: "If damage negation and increased damage dealt is not enough...just summon a few clones and your good to go.",
+    },
+    {
+      Warframe: 'Saryn',
+      Rank: '6',
+      Position: 'At number six',
+      Images: 'images/saryn.jpg',
+      description: "Saryn is not someone to be reckoned with. With her spores seeking enemies, making then take extra damage.",
+    },
+  
+  ];
+
+  function featuredFunction() {
+    let featureCard = '';
+
+
+    projectData.forEach((item) => {
+      const Ranking = item.Rank;
+      const WarframeTitle = item.Warframe;
+      const Images = item.Images;
+      const { Position } = item;
+      const { description } = item;
+      if (Ranking <= 2) {
+        featureCard += `<div class="FeaturedSpeakers col-lg-5 row col-11 offset-lg-1 offset-1">
+        <img src="${Images}" alt="" class="FrameImage col-lg-3 col-4 col-sm-4">
+        <div class="col-lg-6 col-6 col-sm-6">
+            <h2 class="Name col-lg-12 col-12 col-sm-12">${WarframeTitle}</h2>
+            <p class="Position col-lg-12 col-12 col-sm-12">${Position}</p>
+            <p class="Desc col-lg-12 col-12 col-sm-12">${description}</p>
+        </div>
+      </div>`;
+      } else {
+        featureCard += `<div class="FeaturedSpeakers hide col-lg-5 row col-11 offset-lg-1 offset-1">
+        <img src="${Images}" alt="" class="FrameImage col-lg-3 col-4 col-sm-4">
+        <div class="col-lg-6 col-6 col-sm-6">
+            <h2 class="Name col-lg-12 col-12 col-sm-12">${WarframeTitle}</h2>
+            <p class="Position col-lg-12 col-12 col-sm-12">${Position}</p>
+            <p class="Desc col-lg-12 col-12 col-sm-12">${description}</p>
+        </div>
+      </div>`;
+      }
+
+    });
+    document.getElementById('WarframeList Show').innerHTML = featureCard;
+  }
+  featuredFunction();
 
 hamburgerButton.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden');
